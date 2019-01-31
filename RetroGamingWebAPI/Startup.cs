b713d7d8b42d2743d97a781a4cf994c816526349
Alex Thissen <athissen@xpirit.com>
2019-01-31 18:45:55 +0100

    Prometheus and Grafana complete

--- RetroGamingWebAPI/Startup.cs
+++ RetroGamingWebAPI/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
@@ -30,12 +31,14 @@ namespace RetroGamingWebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             string key = Configuration["ApplicationInsights:InstrumentationKey"];
+
+            //services.AddHealthChecksUI();
             services
                 .AddHealthChecks()
                 .AddApplicationInsightsPublisher(key)
+                .AddPrometheusGatewayPublisher("http://pushgateway:9091/metrics", "pushgateway")
                 .AddCheck<RandomHealthCheck>("random", failureStatus: HealthStatus.Degraded);
                // .AddCheck<SqlServerHealthCheck>("sql");
-
 
             services.AddSingleton<SqlServerHealthCheck>(new SqlServerHealthCheck(
                 new SqlConnection(Configuration.GetConnectionString("Test"))));
@@ -59,8 +62,16 @@ namespace RetroGamingWebAPI
             HealthCheckOptions options = new HealthCheckOptions();
             options.ResultStatusCodes[HealthStatus.Unhealthy] = 418;
             options.AllowCachingResponses = true;
+            options.Predicate = _ => true;
+            options.ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse;
 
             app.UseHealthChecks("/health", options);
+            //app.UseHealthChecksUI();
+            //app.UseHealthChecksUI(setup =>
+            //{
+            //    setup.UIPath = "/show-health-ui"; // this is ui path in your browser
+            //    setup.ApiPath = "/health-ui-api"; // the UI ( spa app )  use this path to get information from the store ( this is NOT the healthz path, is internal ui api )
+            //});
             app.UseHttpsRedirection();
             app.UseMvc();
         }
