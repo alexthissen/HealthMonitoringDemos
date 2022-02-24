678551c436995124042b9ccceaffba3f388f8ba8
Roy Cornelissen <rcornelissen@xpirit.com>
2022-02-24 13:45:10 +0100

    👽 Remove obsolete KeyVault package

--- RetroGamingWebAPI/Program.cs
+++ RetroGamingWebAPI/Program.cs
@@ -1,14 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using System.Reflection;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace RetroGamingWebAPI
 {
