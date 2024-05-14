dcce6c13c9adb273691a9c72127f4679dc7e50cc
Alex Thissen <athissen@killer-apps.nl>
2024-05-14 09:09:02 +0200

    Working setup for Development and Production

--- ASPNETCoreHealthMonitoring.AppHost/Program.cs
+++ ASPNETCoreHealthMonitoring.AppHost/Program.cs
@@ -1,19 +1,34 @@
 using Aspire.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.Xml.Linq;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
 var seq = builder
     .AddSeq("seq")
         .WithHttpEndpoint(port: 5341, targetPort: 80, name: "seqlog");
+var postgres = builder
+    .AddPostgres("postgres")
+    .AddDatabase("gaming");
+
+if (builder.Environment.IsProduction())
+{
+    builder.AddContainer("healthchecks", "xabarilcoding/healthchecksui")
+        .WithEnvironment("HEALTHCHECKSUI_HEALTHCHECKSUIPATH", "/healthchecks")
+        .WithEnvironment("HEALTHCHECKSUI_HEALTHCHECKSUIPORT", "80")
+        .WithEnvironment("HealthChecksUI__HealthChecks__0__Uri", "http://host.docker.internal:8080/health/ready")
+        .WithEnvironment("HealthChecksUI__HealthChecks__0__Name", "Readiness checks")
+        .WithHttpEndpoint(port: 8081, targetPort: 80);
+}
 
 builder
     .AddProject<Projects.RetroGamingWebAPI>("retrogamingwebapi")
-        .WithReplicas(1)
-        .WithHttpEndpoint(port: 5000, name: "retrogamingwebapi")
-        .WithHttpsEndpoint(port: 5001, name: "retrogamingwebapi-secure")
-        .WithHttpEndpoint(port: 5002, name: "management")
-
         .WithEnvironment("HEALTH_INITIAL_STATE", "Degraded")
-    .WithReference(seq);
+        .WithReplicas(1)
+    //.WithHttpEndpoint(port: 5000, name: "retrogamingwebapi")
+    //.WithHttpsEndpoint(port: 5001, name: "retrogamingwebapi-secure")
+    //.WithHttpEndpoint(port: 5002, name: "management")
+    .WithReference(seq)
+    .WithReference(postgres);
 
 builder.Build().Run();
